af8cce60387dba6ea1b2738beaf74951a227243d
Henrique Kalife <henriquekalife@gmail.com>
2019-06-26 20:53:05 -0300

    fix(walther-ppk): aim shooting problem fixed
    hopefully the last

--- Assets/Scripts/Weapons/WaltherPPK.cs
+++ Assets/Scripts/Weapons/WaltherPPK.cs
@@ -65,40 +65,6 @@ public class WaltherPPK : MonoBehaviour
 
     void Aim()
     {
-        /*
-        bool processAim = false;
-        if (Input.GetMouseButtonDown(1) && (isReloading == false))
-        {
-            processAim = true;
-        }
-
-        if (processAim)
-        {
-            isAiming = true;
-            Debug.Log("Aim");
-            anim.Play("walther aim");
-            anim.SetBool("aimGun", true);
-
-            if (Input.GetMouseButtonDown(0) && (isAiming == true) && (isReloading == false))
-            {
-                anim.Play("walther aim shoot");
-                anim.SetBool("isShootingAim", true);
-            }
-            if (Input.GetMouseButtonUp(0) && (isAiming == false))
-            {
-                anim.SetBool("isShootingAim", false);
-            }
-        }
-        if (Input.GetMouseButtonUp(1))
-        {
-            isAiming = false;
-            Debug.Log("Return to normal position");
-            anim.SetBool("isShootingAim", false);
-            anim.Play("walther aim return");
-            anim.SetBool("aimGun", false);
-        }
-        */
-
         if (Input.GetMouseButtonDown(1)) //is aiming
         {
             Debug.Log("Is aiming.");
@@ -114,10 +80,19 @@ public class WaltherPPK : MonoBehaviour
             anim.SetBool("aimGun", false);
         }
 
-        if ((isAiming == true) && Input.GetMouseButtonDown(0))
+        if ((isAiming == true) && Input.GetMouseButtonDown(0)) //shoot while aiming
         {
             Debug.Log("Shooting with aiming.");
-            anim.SetBool("isShootingAim", true);
+            anim.Play("walther aim shoot");
+
+            Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log("Hit: " + hitInfo.transform.name);
+            }
+
         }
         if ((isAiming == true) && Input.GetMouseButtonUp(0))
         {
@@ -141,4 +116,5 @@ public class WaltherPPK : MonoBehaviour
             Debug.Log("Finished reloading");
         }
     }
+
 }
