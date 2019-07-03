b192fc72afe67bd92db0cfbb2c8b969848d80d37
Henrique Kalife <henriquekalife@gmail.com>
2019-07-02 22:28:04 -0300

    feat(AI) - test for AI

---
+++ Assets/Scripts/AreaBox.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaBox : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text areaTexto;
+
+    [SerializeField]
+    public bool colidindo = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player está na " + this.gameObject.name);
+            areaTexto.text = this.gameObject.name;
+            colidindo = true;
+        }
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player está na " + this.gameObject.name);
+            areaTexto.text = this.gameObject.name;
+            colidindo = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        colidindo = false;
+    }
+
+}
---
+++ Assets/Scripts/Tempo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tempo : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text tempoTexto;
+
+    private int tempoInicial = 10;
+
+    private int tempoAtual;
+
+    private AreaBox areaBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempoAtual = tempoInicial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator atualizaTempo () {
+        string stringTempo = tempoAtual.ToString();
+        tempoTexto.text = stringTempo;
+        yield return new WaitForSeconds(1f);
+        tempoAtual = tempoAtual - 1;
+    }
+
+}
