2a9bbc94de4cae0d782cff0b11171ebd9c1d819f
Henrique Kalife <henriquekalife@gmail.com>
2019-07-28 21:26:54 -0300

    feat(enemy) - mesh collider

---
+++ Assets/Scripts/Enemy Script/EnemyAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnim : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.SetBool("started", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
